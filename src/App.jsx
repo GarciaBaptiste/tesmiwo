57995390e023cdb6df790b3788c75d0dc8c8a6c1
GarciaBaptiste <baptiste1garcia@gmail.com>
2024-10-15 13:17:13 +0200

    feat: better -s- shape

--- src/App.jsx
+++ src/App.jsx
@@ -514,8 +514,8 @@ const LetterT = () => {
 const LetterS = () => {
   return (
     <BasicLetter style={{ gridTemplateColumns: "var(--l) var(--m) 1fr 1fr var(--l) var(--m)", gridTemplateRows: "var(--s) 0.5fr 0.5fr var(--m) var(--l) 1fr var(--s) 1fr var(--s)", flex: "0.7" }} $offsety={true}>
-      <Round style={{ gridArea: "1 / 1 / 3 / 4" }} orientation="tl" $strokex="var(--l)" $strokey="var(--s)" $square={true} $thickerx={true} />
-      <Strok style={{ gridArea: "1 / 4 / 2 / 6" }} />
+      <Round style={{ gridArea: "1 / 1 / 3 / 3" }} orientation="tl" $strokex="var(--l)" $strokey="var(--s)" $square={true} $thickerx={true} />
+      <Strok style={{ gridArea: "1 / 3 / 2 / 6" }} />
       <Serif style={{ gridArea: "2 / 5 / 3 / 6" }} orientation="vtr"/>
       <Round style={{ gridArea: "3 / 1 / 6 / 4" }} orientation="bl" $strokex="var(--l)" $strokey="var(--l)" $square={true} $thickerx={true} />
       <Round style={{ gridArea: "5 / 4 / 7 / 7" }} orientation="tr" $strokex="var(--l)" $strokey="var(--l)" $square={true} $thickerx={true} />
